54b762f2ecb1b5b75ed42c37e4011c1b0f42b0f2
vedant-11 <69393820+vedant-11@users.noreply.github.com>
2022-01-22 03:59:45 +0530

    created token

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Wallet from "./Wallet";
+import wallet from "./Wallet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Create your own token using JavaScript</h1>
+      <Wallet />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Wallet.jsx
@@ -0,0 +1,234 @@
+import React, { useEffect, useState } from "react";
+import {
+  Connection,
+  clusterApiUrl,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import {
+  Keypair,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
+const Wallet = () => {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [provider, setProvider] = useState();
+  const [loading, setLoading] = useState();
+  const [isTokenCreated, setIsTokenCreated] = useState(false);
+  const [createdTokenPublicKey, setCreatedTokenPublicKey] = useState(null);
+  const [mintingWalletSecretKey, setMintingWalletSecretKey] = useState(null);
+  const [supplyCapped, setSupplyCapped] = useState(false);
+  const getProvider = async () => {
+    if ("solana" in window) {
+      const provider = window.solana;
+      if (provider.isPhantom) {
+        return provider;
+      }
+    } else {
+      window.open("https://www.phantom.app/", "_blank");
+    }
+  };
+  const airDropHelper = async () => {
+    try {
+      setLoading(true);
+      const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+      const fromAirDropSignature = await connection.requestAirdrop(
+        new PublicKey(provider.publicKey),
+        LAMPORTS_PER_SOL
+      );
+      await connection.confirmTransaction(fromAirDropSignature, {
+        commitment: "confirmed",
+      });
+
+      console.log(
+        `1 SOL airdropped to your wallet ${provider.publicKey.toString()} successfully`
+      );
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  const walletConnectionHelper = async () => {
+    if (walletConnected) {
+      //Disconnect Wallet
+      setProvider();
+      setWalletConnected(false);
+    } else {
+      const userWallet = await getProvider();
+      if (userWallet) {
+        await userWallet.connect();
+        userWallet.on("connect", async () => {
+          setProvider(userWallet);
+          setWalletConnected(true);
+        });
+      }
+    }
+  };
+
+  const initialMintHelper = async () => {
+    try {
+      setLoading(true);
+      const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+      const mintRequester = await provider.publicKey;
+      const mintingFromWallet = await Keypair.generate();
+      setMintingWalletSecretKey(JSON.stringify(mintingFromWallet.secretKey));
+
+      const fromAirDropSignature = await connection.requestAirdrop(
+        mintingFromWallet.publicKey,
+        LAMPORTS_PER_SOL
+      );
+      await connection.confirmTransaction(fromAirDropSignature, {
+        commitment: "confirmed",
+      });
+
+      const creatorToken = await Token.createMint(
+        connection,
+        mintingFromWallet,
+        mintingFromWallet.publicKey,
+        null,
+        6,
+        TOKEN_PROGRAM_ID
+      );
+      const fromTokenAccount =
+        await creatorToken.getOrCreateAssociatedAccountInfo(
+          mintingFromWallet.publicKey
+        );
+      await creatorToken.mintTo(
+        fromTokenAccount.address,
+        mintingFromWallet.publicKey,
+        [],
+        1000000
+      );
+
+      const toTokenAccount =
+        await creatorToken.getOrCreateAssociatedAccountInfo(mintRequester);
+      const transaction = new Transaction().add(
+        Token.createTransferInstruction(
+          TOKEN_PROGRAM_ID,
+          fromTokenAccount.address,
+          toTokenAccount.address,
+          mintingFromWallet.publicKey,
+          [],
+          1000000
+        )
+      );
+      const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [mintingFromWallet],
+        { commitment: "confirmed" }
+      );
+
+      console.log("SIGNATURE:", signature);
+
+      setCreatedTokenPublicKey(creatorToken.publicKey.toString());
+      setIsTokenCreated(true);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  const mintAgainHelper = async () => {
+    try {
+      setLoading(true);
+      const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+      const createMintingWallet = await Keypair.fromSecretKey(
+        Uint8Array.from(Object.values(JSON.parse(mintingWalletSecretKey)))
+      );
+      const mintRequester = await provider.publicKey;
+
+      const fromAirDropSignature = await connection.requestAirdrop(
+        createMintingWallet.publicKey,
+        LAMPORTS_PER_SOL
+      );
+      await connection.confirmTransaction(fromAirDropSignature, {
+        commitment: "confirmed",
+      });
+
+      const creatorToken = new Token(
+        connection,
+        createdTokenPublicKey,
+        TOKEN_PROGRAM_ID,
+        createMintingWallet
+      );
+      const fromTokenAccount =
+        await creatorToken.getOrCreateAssociatedAccountInfo(
+          createMintingWallet.publicKey
+        );
+      const toTokenAccount =
+        await creatorToken.getOrCreateAssociatedAccountInfo(mintRequester);
+      await creatorToken.mintTo(
+        fromTokenAccount.address,
+        createMintingWallet.publicKey,
+        [],
+        100000000
+      );
+
+      const transaction = new Transaction().add(
+        Token.createTransferInstruction(
+          TOKEN_PROGRAM_ID,
+          fromTokenAccount.address,
+          toTokenAccount.address,
+          createMintingWallet.publicKey,
+          [],
+          100000000
+        )
+      );
+      await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [createMintingWallet],
+        { commitment: "confirmed" }
+      );
+
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {walletConnected ? (
+        <p>
+          <strong>Public Key:</strong> {provider.publicKey.toString()}
+        </p>
+      ) : (
+        <p></p>
+      )}
+      <button onClick={walletConnectionHelper} disabled={loading}>
+        {!walletConnected ? "Connect Wallet" : "Disconnect Wallet"}
+      </button>{" "}
+      {walletConnected ? (
+        <p>
+          Create your own token
+          <button disabled={loading} onClick={initialMintHelper}>
+            Initial Mint{" "}
+          </button>
+        </p>
+      ) : (
+        <></>
+      )}
+      <li>
+        Mint More 100 tokens:{" "}
+        <button disabled={loading || supplyCapped} onClick={mintAgainHelper}>
+          Mint Again
+        </button>
+      </li>
+      <li>
+        Transfer to friend:{" "}
+        <button disabled={loading || supplyCapped}>Transfer to friend</button>
+      </li>
+    </>
+  );
+};
+
+export default Wallet;
